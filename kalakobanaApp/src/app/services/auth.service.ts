ef46fd8bbfb264e7d5be82c40a73f9bb868a8d4b
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-21 09:03:59 +0400

    Login Button Hooked Up

--- kalakobanaApp/src/app/services/auth.service.ts
+++ kalakobanaApp/src/app/services/auth.service.ts
@@ -11,13 +11,18 @@ export class AuthService {
     const settings = {
       authority: 'https://localhost:5001',  
       client_id: 'kalakobanaBFF',          
-      redirect_uri: 'http://localhost:4200/callback',  
+      redirect_uri: 'https://localhost:7250/signin-oidc',  
       post_logout_redirect_uri: 'http://localhost:4200/',
       response_type: 'code',
       scope: 'openid profile kalakobanaapi.read kalakobanaapi.write roles subscription',
       silent_redirect_uri: 'http://localhost:4200/silent-renew',
       automaticSilentRenew: true
     };
+    this.userManager = new UserManager(settings);
+    // Load user from session storage if available
+    this.userManager.getUser().then(user => {
+      this.user = user;
+    });
   }
   login(): void {
     this.userManager.signinRedirect();
@@ -34,4 +39,14 @@ export class AuthService {
   getAccessToken(): string | null {
     return this.user?.access_token ?? null;
   }
+  handleLoginCallback(): Promise<void> {
+    return this.userManager.signinRedirectCallback().then(user => {
+      this.user = user;
+    });
+  }
+
+  // Handle callback after logout
+  // handleLogoutCallback(): Promise<void> {
+  //   return this.userManager.signoutRedirectCallback();
+  // }
 }
--- kalakobanaApp/src/app/welcome/welcome.component.ts
+++ kalakobanaApp/src/app/welcome/welcome.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {  faBars, faSignInAlt, faTimes, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,17 +11,30 @@ import {  faBars, faSignInAlt, faTimes, faUserPlus } from '@fortawesome/free-sol
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit{
+  ngOnInit(): void {
+    this.authService.handleLoginCallback().then(() => {
+      console.log('Login successful');
+      this.router.navigate(['/game-hub']);
+    }).catch(err => {
+      console.error('Error during login callback', err);
+    });
+  }
   faBar = faBars;
   faUserPlus = faUserPlus;
   faSignInAlt = faSignInAlt;
   navbarOpen = false;
   faTimes = faTimes;
+  authService = inject(AuthService);
+  router = inject(Router);
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
   login(){
+    this.authService.login();
+    
   }
   register(){
+    this.authService.logout();
   }
 }
