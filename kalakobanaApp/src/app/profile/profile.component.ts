356b067b7cfa9ec8c12065e2a8289db27e0cb8d7
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-24 16:56:18 +0400

    FORM ADDED

--- kalakobanaApp/src/app/profile/profile.component.ts
+++ kalakobanaApp/src/app/profile/profile.component.ts
@@ -2,15 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from "../shared/navbar/navbar.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule, NavbarComponent],
+  imports: [FontAwesomeModule, CommonModule, NavbarComponent, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  form = new FormGroup({
+    firstName: new FormControl('',Validators.required),
+    lastName: new FormControl('',Validators.required),
+    userName: new FormControl('',Validators.required)
+  })
 
   showProfileForm = signal(true);
   showSubscriptionForm = signal(false);
@@ -27,5 +33,9 @@ export class ProfileComponent {
       this.showSubscriptionForm.set(true);  
     }
   }
- 
+  onSubmit(){
+    console.log(this.form.value.firstName);
+    console.log(this.form.value.lastName);
+    console.log(this.form.value.userName);
+  }
 }
