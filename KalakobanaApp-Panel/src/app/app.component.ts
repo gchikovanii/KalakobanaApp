acb01560779587027dffc968cfd01097b01da095
gchikovnaii25 <gchikovanii25@gmail.com>
2024-10-01 15:54:55 +0400

    redirection

--- KalakobanaApp-Panel/src/app/app.component.ts
+++ KalakobanaApp-Panel/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "./main/main.component";
 import { SidenavComponent } from "./shared/sidenav/sidenav.component";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, SidenavComponent],
+  imports: [RouterOutlet, MainComponent, SidenavComponent,  MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'KalakobanaApp-Panel';
+  loggedIn = false;
 }
