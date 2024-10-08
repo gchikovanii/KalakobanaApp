78fadadff6cee87b68e588ce246e5b78707eb263
gchikovnaii25 <gchikovanii25@gmail.com>
2024-10-08 22:47:00 +0400

    hub added into microservice

--- kalakobanaApp/src/app/services/room.service.ts
+++ kalakobanaApp/src/app/services/room.service.ts
@@ -4,12 +4,14 @@ import { Observable } from 'rxjs';
 import { Room } from '../Models/room';
 import { CreateRoomRequest } from '../Models/createRoomRequest';
 import { RoomResponse } from '../Models/roomRespose';
+import * as signalR from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
   private apiUrl = 'https://localhost:7250/bff/room'; 
+  private hubConnection: signalR.HubConnection | undefined;
 
   constructor(private http: HttpClient) {}
 
@@ -28,5 +30,27 @@ export class RoomService {
   // Remove a room by name
   deleteRoomByName(roomName: string): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/by-name/${roomName}`, { withCredentials: true });
+  }
+
+  startHubConnection(roomName: string): Promise<void> {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl(`https://localhost:7250/hub/room?roomName=${roomName}`, {
+        withCredentials: true
+      })
+      .build();
+
+    return this.hubConnection.start()
+      .then(() => console.log('SignalR connection established for room:', roomName))
+      .catch(err => console.error('Error establishing SignalR connection:', err));
+  }
+
+  // Stop SignalR connection
+  stopHubConnection(): Promise<void> {
+    return this.hubConnection?.stop() || Promise.resolve();
+  }
+
+  // Join room signal handler
+  joinRoom(roomName: string): Promise<void> {
+    return this.hubConnection?.invoke('JoinRoom', roomName) || Promise.reject('No connection');
   }
 }
